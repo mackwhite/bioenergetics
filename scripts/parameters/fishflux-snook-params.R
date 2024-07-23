3c6d3d3cf855909028952c4cb78e573356990367
mackwhite <mwhite@fiu.edu>
2024-07-22 23:09:37 -0400

    playing with the data

--- scripts/parameters/fishflux-snook-params.R
+++ scripts/parameters/fishflux-snook-params.R
@@ -18,12 +18,12 @@ data(param_zebsco)
 # aspect ratio ------------------------------------------------------------
 ### source = FishBase.org
 asp_rat <- aspect_ratio("Centropomus undecimalis")
-r = 1.90693
+# r = 1.90693
 
 # trophic level -----------------------------------------------------------
 
 troph_lev <- trophic_level("Centropomus undecimalis")
-h = 4.125
+# h = 4.125
 
 # length-weight relationship ----------------------------------------------
 ### source = Froese, R., J. Thorson and R.B. Reyes Jr., 2013. A Bayesian approach for estimating length-weight relationships in fishes. J. Appl. Ichthyol. (2013):1-7
@@ -31,10 +31,10 @@ lwa_lwb <- find_lw("Centropomus undecimalis")
 # species                     lwa_m    lwa_sd    lwb_m  lwb_sd
 # 1 Centropomus undecimalis 0.00741 0.000994898  3.02 0.0255102
 
-lwa_m = 0.00741
-lwa_sd = 0.000994898
-lwb_m = 3.02
-lwb_sd = 0.0255102
+# lwa_m = 0.00741
+# lwa_sd = 0.000994898
+# lwb_m = 3.02
+# lwb_sd = 0.0255102
 
 ### use shark river values, but may need to be redone since fishflux used g/cm
 
@@ -42,9 +42,9 @@ lwb_sd = 0.0255102
 ### source = FishBase.org
 ### use Taylor et al 2000 estimates but need to determine if those need to be recalculated bc 
 ### fisflux uses tl(cm) and 
-linf_k_t0 <- growth_params("Centropomus undecimalis", otolith = TRUE)
-linf <- 77.7
-k <- 0.49
+# linf_k_t0 <- growth_params("Centropomus undecimalis", otolith = TRUE)
+# linf <- 77.7
+# k <- 0.49
 
 # metabolic parameters ----------------------------------------------------
 
@@ -70,16 +70,15 @@ model_parameters(sp = "Macrognathus siamensis", family = "Mastacembelidae", temp
 model_parameters(sp = "Mugil cephalus", family = "Mugilidae", temp = 24.1825, otolith = FALSE)
 model_parameters(sp = "Pterygoplichthys multiradiatus", family = "Loricariidae", temp = 24.1825, otolith = FALSE)
 
-
 # metabolism --------------------------------------------------------------
 ### source = Barneche & Allen 2018 Ecology Letters doi: 10.1111/ele.12947. These parameters are for the best model (Model 2 in the paper online supplementary material) of fish resting metabolic rates reported in the paper, which also includes trophic level as a covariate.
 metabolism("Centropomidae", temp = 24.1825, troph_m = 4.125)
-f0_m = 0.002183832
-f0_sd = 4.491014e-10  
-alpha_m = 0.77
-alpha_sd = 0.05286288
-b0_m = 0.00048083
-b0_sd = 1e-10
+# f0_m = 0.002183832
+# f0_sd = 4.491014e-10  
+# alpha_m = 0.77
+# alpha_sd = 0.05286288
+# b0_m = 0.00048083
+# b0_sd = 1e-10
 
 metabolism("Sciaenidae", temp = 24.1825, troph_m = 4.125)
 metabolism("Centrarchidae", temp = 24.1825, troph_m = 4.125)
@@ -102,15 +101,18 @@ mod <- cnp_model_mcmc(TL = 25:75, param = list(
       r_m = 1.906930, h_m = 4.125, h_sd = 0.5, v_m = 24.1825, v_sd = 3.449313 
       ))
 
-plot_cnp(mod = mod, y = c("Fp"),
+plot_cnp(mod = mod, y = c("Fp", "Ip", "Fn", "In", "Fc", "Ic"),
+         x = "tl", probs = c(0.5))
+
+plot_cnp(mod = mod, y = c("Fp", "Ip", "Fn", "In"),
+         x = "tl", probs = c(0.8))
+
+plot_cnp(mod = mod, y = c("Fp", "Ip"),
          x = "tl", probs = c(0.5, 0.8))
 
 plot_cnp(mod = mod, y = c("Fp", "Ip"),
          x = "tl", probs = c(0.5))
 
-sensitivity(TL = 10, param = list(Qc_sd = 2.71, Qn_sd = 0.75, Qp_sd = 0.13,
-                                  Dc_sd = 3.974171, Dn_sd = 0.855285, Dp_sd = 0.634503),
-            par = c("Qp_sd","Qn_sd","Qc_sd",
-                    "Dp_sd","Dn_sd","Dc_sd"), out = c("Fp", "Ip"))
+all <- data.frame(mod$summary)
 
-      
+test <- read_rds("../shark-river-movement-syndromes/data/snook_cnd_movement_clean.rds")
